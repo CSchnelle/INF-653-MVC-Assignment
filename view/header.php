6bdb560ab53687e3103773af488d6041808deb3e
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-02-27 00:09:08 -0600

    first commit

---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> TO DO LIST</title>
+		<link rel ="stylesheet" type="text/css"
+		      href="main.css">
+	</head>
+	<body>
+		<header>
+			<h1>TO DO LIST</h1>
+		</header>
+		
