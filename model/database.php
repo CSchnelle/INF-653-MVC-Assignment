d2ca9ca7d5b8508e4f85d66b4278ce3ef5117ed4
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-03-18 02:21:44 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,14 +1,14 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=todolist';
-    $username = 'root';
-    //$password = 'pa55word';
+$dsn = 'mysql:host=qbhol6k6vexd5qjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=w037hv168sn3tjez';
+    $username = 'u8lequiy705eacm8';
+    $password = 'cu1c97oi6oa2bf0v';
+//$username = 'root';
 
-    try {
-        $db = new PDO($dsn, $username);
-        //$db = new PDO($dsn, $username, $password);
-    } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        include('database_error.php');
-        exit();
-    }
+try {
+  $db = new PDO($dsn, $username, $password);
+} catch (PDOException $e) {
+  $error_message = $e->getMessage();
+  include('database_error.php');
+  exit();
+}
 ?>
